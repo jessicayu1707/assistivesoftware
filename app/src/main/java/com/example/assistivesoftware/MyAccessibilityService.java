71022e014e15a51c11b3a571237bc63ce9d6980c
Jessica Yu <jessica.yu-2@student.manchester.ac.uk>
2019-02-15 14:27:28 +0000

    Added accessibility class and corresponding tags in xml and manifest

---
+++ app/src/main/java/com/example/assistivesoftware/MyAccessibilityService.java
@@ -0,0 +1,16 @@
+package com.example.assistivesoftware;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+
+}
