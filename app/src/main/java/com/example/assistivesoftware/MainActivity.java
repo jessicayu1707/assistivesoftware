d65c0cbc6451e6853147e6f97a67c56272047b58
Jessica Yu <jessica.yu-2@student.manchester.ac.uk>
2019-02-18 19:21:12 +0000

    Can now speak (hardcoded) - checks there is resource available on device - downloads if needed - initialise the TTS function - speaks a hardcoded line

--- app/src/main/java/com/example/assistivesoftware/MainActivity.java
+++ app/src/main/java/com/example/assistivesoftware/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.assistivesoftware;
 
+import android.content.Context;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    static int MY_DATA_CHECK_CODE = 1; //from example
+    public void checkTTS(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Hello Check!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
+        //onActivityResult(MY_DATA_CHECK_CODE,resultCode,checkIntent);
+    }
+
+    private TextToSpeech mTts;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello! Im on activity result now!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                // success, create the TTS instance
+                mTts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        mTts.setLanguage(Locale.UK);
+                    }
+                });
+            } else {
+                // missing data, install it
+                Intent installIntent = new Intent();
+                installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+            }
+        }
+    }
+
+    //called when user taps on the button
+    public void speakTTS(View view){
+        //do something in response to clicking send button
+        Context context = getApplicationContext();
+        CharSequence text = "Hello speak tts!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        String toSpeak = "Hello I am working.";
+        mTts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+        //make intent (where does it go?)
+        //Intent intent = new Intent(this, Accessibility.class);
+        //start the intent
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/assistivesoftware/MyAccessibilityService.java
+++ app/src/main/java/com/example/assistivesoftware/MyAccessibilityService.java
@@ -1,12 +1,31 @@
 package com.example.assistivesoftware;
 
 import android.accessibilityservice.AccessibilityService;
+import android.speech.tts.TextToSpeech;
 import android.view.accessibility.AccessibilityEvent;
+
+import java.util.Locale;
 
 public class MyAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        final int eventType = event.getEventType();
+        String eventText = null;
+        switch(eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "Clicked: ";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                eventText = "Focused: ";
+                break;
+        }
+
+        eventText = eventText + event.getContentDescription();
+
+        // Do something nifty with this text, like speak the composed string
+        // back to the user.
+        //speakToUser(eventText);
     }
 
     @Override
